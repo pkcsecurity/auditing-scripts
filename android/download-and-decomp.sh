eefeaeb727710fd91ddaf826f92f7b5acfbf1907
Joshua Maddux <josh@pkcsecurity.com>
2019-05-13 13:40:44 -0700

    Download apks to separate directories

--- android/download-and-decomp.sh
+++ android/download-and-decomp.sh
@@ -1,10 +1,11 @@
 APP_ID=$1
 set -e
 echo "Downloading $APP_ID..."
-rm -r target/ 2>/dev/null || echo "No target directory, nothing to clean"
-mkdir target
-pushd target
-gplaycli -d "$APP_ID" -c ../gplaycli.conf
+rm -r "target/$APP_ID" 2>/dev/null || echo "No target directory, nothing to clean"
+mkdir -p "target/$APP_ID"
+pushd "target/$APP_ID"
+gplaycli -d "$APP_ID" -c ../../gplaycli.conf
 d2j-dex2jar -f *.apk
-java -jar ../fernflower.jar *.jar decomp/
+java -jar ../../fernflower.jar *.jar decomp/
+unzip decomp/*.jar
 popd
